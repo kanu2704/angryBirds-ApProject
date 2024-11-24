3c8b4ccbbb2598143b0a3c68bfc16a52433cfa7f
kanu2704 <kanu13.gupta@gmail.com>
2024-11-24 16:00:32 +0530

    bird launch and speed of the game improved

---
+++ core/src/main/java/com/ap_project/game/states/collisionHandler.java
@@ -0,0 +1,28 @@
+package com.ap_project.game.states;
+
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Manifold;
+
+public class collisionHandler implements ContactListener {
+    @Override
+    public void beginContact(Contact contact) {
+
+    }
+
+    @Override
+    public void endContact(Contact contact) {
+
+    }
+
+    @Override
+    public void preSolve(Contact contact, Manifold manifold) {
+
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse contactImpulse) {
+
+    }
+}
